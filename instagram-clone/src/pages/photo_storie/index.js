3e526f87b79d52e7965104704117134da2f82780
Bruno Macêdo <73453125+brunomalenco@users.noreply.github.com>
2023-05-30 22:20:37 -0300

    minimas atualizacoes

---
+++ instagram-clone/src/pages/photo_storie/index.js
@@ -0,0 +1,9 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export default function PhotoStorie(){
+    return(
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            Foto
+        </View>
+    )
+}
